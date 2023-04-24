deb25b9a1a465e616004ba9e833db2e59eab9a81
deltechdesigns <deltechdesigns@gmail.com>
2023-04-24 17:50:43 +0100

    another srceen

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import "./App.css";
 import Page from "./";
 import { RiseLoader } from "react-spinners";
 import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import SiteMap from "./sitemap";
+import Header from "./header";
+
 function App() {
   const [loading, setLoading] = useState(false);
   const pageLoader = () => {
@@ -22,7 +26,13 @@ function App() {
           <RiseLoader color="#000000" />{" "}
         </div>
       ) : (
-        <Page />
+        <Routes>
+            <Route path="/*" element={<SiteMap />} />
+          <Route path="/" element={<Header />}>
+            <Route path="/" element={<Page />} />
+            {/* <Route path="/map" element={<SiteMap />} exact /> */}
+          </Route>
+        </Routes>
       )}
     </div>
   );
